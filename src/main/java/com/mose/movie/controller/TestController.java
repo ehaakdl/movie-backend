e72d8594d0426e0096f1ffbb4faf73ec7c0bd2e7
ehaakdl <ehaakdl@naver.com>
2022-04-23 20:11:27 +0900

    refactor: 업로드 파일 경로 수정, application.yml 수정
    application.yml
    - 임시경로 수정및 업로드 경로 속성 추가

--- src/main/java/com/mose/movie/controller/TestController.java
+++ src/main/java/com/mose/movie/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.mose.movie.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +14,16 @@ import java.util.List;
 @RestController
 @Slf4j
 public class TestController {
+    @Value("${movie.path.upload}")
+    private String uploadPath;
+
     @PostMapping("/test")
     public List<String> upload(@RequestPart("file") List<MultipartFile> files) throws Exception {
         List<String> list = new ArrayList<>();
         for (MultipartFile file : files) {
             log.info("파일이름: {}", file.getOriginalFilename());
-            String originalfileName = file.getOriginalFilename();
-            File dest = new File("./test/" + originalfileName);
+            String originalName = file.getOriginalFilename();
+            File dest = new File(uploadPath + originalName);
             file.transferTo(dest);
         }
         return list;
