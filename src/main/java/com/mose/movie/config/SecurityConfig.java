7073627af29e5000287aa9582ce0d85c4e72b84e
ehaakdl <ehaakdl@naver.com>
2022-06-18 20:12:58 +0900

    feat: spring security config 추가
    기본 설정만 구현

---
+++ src/main/java/com/mose/movie/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.mose.movie.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Slf4j
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .anyRequest()
+                        .permitAll()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
