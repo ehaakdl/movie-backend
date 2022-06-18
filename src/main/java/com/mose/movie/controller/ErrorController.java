892c6032a9f4f1cb1bc08d6884f1f8af8c7afbd9
ehaakdl <ehaakdl@naver.com>
2022-06-18 13:25:53 +0900

    refactor: error controlleradvice 수정
    - exception cause 체크 되면 로그는 cause 로 찍게 수정
    - errorResponseException 파라미터
    값 수정(실제 쓰는곳은 controllerAdvice라 굳이 code, message, success를 풀어서 exception에
    담아 둘 필요가없음)

--- src/main/java/com/mose/movie/controller/ErrorController.java
+++ src/main/java/com/mose/movie/controller/ErrorController.java
@@ -4,31 +4,56 @@ import com.mose.movie.etc.define.ErrorResponseException;
 import com.mose.movie.etc.define.ResponseDTO;
 import com.mose.movie.etc.define.eResponseErrorInfo;
 import com.mose.movie.utils.CommonUtils;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Optional;
 
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
+@RequiredArgsConstructor
 public class ErrorController {
 
+    private final CommonUtils commonUtils;
+
     @ExceptionHandler(ErrorResponseException.class)
-    private ResponseEntity<ResponseDTO> apiError(ErrorResponseException e) {
-        log.error(e.getMessage());
+    private ResponseEntity<ResponseDTO> errorResponseException(ErrorResponseException e) {
+        if(e.getCause() == null){
+            log.error(commonUtils.getStackTraceElements(e));
+        } else{
+            log.error(commonUtils.getStackTraceElements(e.getCause()));
+        }
+
+        HttpStatus status = Optional
+                .ofNullable(e.getErrorResponseInfo())
+                .map(eResponseErrorInfo::getHttpStatus)
+                .orElse(null);
+        if(status == null){
+            log.error("잘못된 HTTP status 코드입니다.");
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        String message = Optional
+                .ofNullable(e.getErrorResponseInfo())
+                .map(eResponseErrorInfo::getMessage)
+                .orElse(null);
+
+        boolean success = e.isSuccess();
+
         return ResponseEntity
-                .status(e.getHttpStatus())
+                .status(status)
                 .body(new ResponseDTO(
-                        e.getMessage()
-                        , e.isSuccess()));
+                        message,
+                        success));
     }
 
     @ExceptionHandler(Exception.class)
     private ResponseEntity<ResponseDTO> ExceptionError(Exception e) {
-        log.error(e.getMessage());
-        log.error(CommonUtils.getStackTraceElements(e));
+        log.error(commonUtils.getStackTraceElements(e));
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ResponseDTO(
--- src/main/java/com/mose/movie/utils/CommonUtils.java
+++ src/main/java/com/mose/movie/utils/CommonUtils.java
@@ -1,19 +1,20 @@
 package com.mose.movie.utils;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public final class CommonUtils {
-    public static String getStackTraceElements(Throwable e){
+
+    public String getStackTraceElements(final Throwable e){
         StackTraceElement[] stackElements;
         StringBuilder result = new StringBuilder();
-        if(e.getCause() == null){
-            stackElements = e.getStackTrace();
-        } else {
-            stackElements = e.getCause().getStackTrace();
-        }
-
+        stackElements = e.getStackTrace();
         if (stackElements == null || stackElements.length == 0) {
             return null;
         }
 
+        result.append(e);
+        result.append('\n');
         for (StackTraceElement stackElement : stackElements) {
             result.append(stackElement);
             result.append('\n');
