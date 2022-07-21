697e4254a7969e9756e901c1b283c7f3d152c806
ehaakdl <ehaakdl@naver.com>
2022-07-21 22:30:00 +0900

    refactor: 게시글 개체에서 멤버 단방향 설계로 교체
    멤버 입장에서 게시글 보는 서비스는 아직 구현할 생각 없어서
    단방향 설계로 교체

--- src/main/java/com/mose/movie/domain/member/Member.java
+++ src/main/java/com/mose/movie/domain/member/Member.java
@@ -1,18 +1,14 @@
 package com.mose.movie.domain.member;
 
-import com.mose.movie.domain.board.Board;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
 @Builder(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString
 public class Member {
     @Id
     @GeneratedValue
@@ -30,10 +26,6 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private eMemberJoinType joinType;
 
-    @OneToMany(mappedBy = "member")
-    @Builder.Default
-    private List<Board> boards = new ArrayList<>();
-
     public static Member createMember(final String email, final String memberId
             , final Long point, final String nickname
             , final String memberPasswd
@@ -49,17 +41,4 @@ public class Member {
                 .point(point)
                 .build();
     }
-/*
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Member member = (Member) o;
-        return id.equals(member.id) && email.equals(member.email) && memberId.equals(member.memberId) && point.equals(member.point) && nickname.equals(member.nickname) && memberPasswd.equals(member.memberPasswd) && joinType == member.joinType && boards.equals(member.boards);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, email, memberId, point, nickname, memberPasswd, joinType, boards);
-    }*/
 }
