1cd4e921fd34ef5475510e46d1e7fc81668564c6
ehaakdl <ehaakdl@naver.com>
2022-07-23 13:11:25 +0900

    test: member, board 테스트 케이스 추가
    - 회원 아이디 조회
    - 회원 이메일 조회
    - 회원 탈퇴
    - 회원 소유로 게시글 존재 할 떄 탈퇴

--- src/test/java/com/mose/movie/repository/board/BoardRepositoryTest.java
+++ src/test/java/com/mose/movie/repository/board/BoardRepositoryTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -18,7 +17,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
-@Rollback(value = false)
 class BoardRepositoryTest {
     @Autowired
     BoardRepository boardRepository;
