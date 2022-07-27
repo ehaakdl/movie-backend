1ce22b62bdd2ca3085275b2acb8b44129599f244
ehaakdl <ehaakdl@naver.com>
2022-07-27 22:14:30 +0900

    refactor: File, Movie 개체 생성자 추가
    @Builder 패턴으로 Movie 객체 생성 추가

--- src/main/java/com/mose/movie/domain/file/File.java
+++ src/main/java/com/mose/movie/domain/file/File.java
@@ -1,23 +1,33 @@
 package com.mose.movie.domain.file;
 
+import com.mose.movie.domain.board.Board;
 import lombok.*;
 
 import javax.persistence.*;
 
 @Getter
 @Entity
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "file_type")
 @Table(name = "File")
 public abstract class File {
+
+    public File(Long fileSz, String originName, String identifyName
+            , String filePath, Board board) {
+        this.fileSz = fileSz;
+        this.originName = originName;
+        this.identifyName = identifyName;
+        this.filePath = filePath;
+        this.board = board;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "file_size")
-    private long fileSz;
+    private Long fileSz;
 
     @Column(name = "origin_name")
     private String originName;
@@ -28,6 +38,7 @@ public abstract class File {
     @Column(name = "file_path")
     private String filePath;
 
-    @Column(name = "stream_link")
-    private String streamLink;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_identify")
+    private Board board;
 }
--- src/main/java/com/mose/movie/domain/file/Movie.java
+++ src/main/java/com/mose/movie/domain/file/Movie.java
@@ -1,17 +1,24 @@
 package com.mose.movie.domain.file;
 
-import lombok.Getter;
+import com.mose.movie.domain.board.Board;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Entity
 @DiscriminatorValue("STREAM")
 @Getter
-@Table(name = "File")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class Movie extends File{
     @Column(name = "stream_link")
     private String streamLink;
+
+    @Builder
+    public Movie(Long fileSz, String originName, String identifyName, String filePath, Board board, String streamLink) {
+        super(fileSz, originName, identifyName, filePath, board);
+        this.streamLink = streamLink;
+    }
 }
