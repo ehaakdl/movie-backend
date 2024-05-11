dd7fbd7e89ef1765ed4da29d52d0ae7889e06719
mosehun <ehaakdl@naver.com>
2024-05-11 14:15:14 +0900

    docs: todo 작성

--- src/main/java/com/movie/backend/service/notice/NoticeService.java
+++ src/main/java/com/movie/backend/service/notice/NoticeService.java
@@ -1,5 +1,7 @@
 package com.movie.backend.service.notice;
-
+// TODO 알림수단을 하나의 타입으로 wrapping 시켜야함 사용할때
+// slack, email, kakaotalk 등 NoticeService 타입으로 다 받을 수 있어야한다.
+// 필요한 서비스 스케쥴러에서 등록된 모든 알림 서비스로 보낼 수 있어야한다.
 public interface NoticeService {
     public void notice();
 }
